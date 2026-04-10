bae148554126782314a9278b199b4c635bad7540
Gonçalo Renha Fernandes <goncalo.fernandes@byme.pt>
2026-04-11 00:52:39 +0100

    Fix plugin entry point to use relative import

--- tools/analyzer_plugin/bin/plugin.dart
+++ tools/analyzer_plugin/bin/plugin.dart
@@ -1,7 +1,7 @@
 import 'dart:isolate';
 
 import 'package:analyzer_plugin/starter.dart';
-import 'package:enforced_lints_plugin/src/plugin.dart';
+import '../lib/src/plugin.dart';
 
 void main(List<String> args, SendPort sendPort) {
   ServerPluginStarter(EnforcedLintsPlugin()).start(sendPort);
